4d777f1b721bfeea858c6ab02aa5699631f5b5b8
vitalij99 <vitalisklymko@gmail.com>
2025-10-22 20:01:31 +0300

    init

---
+++ New Userscript-2025-10-22.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-10-22
+// @description  try to take over the world!
+// @author       You
+// @match        https://web.usagi.one/genius_artist_s_random_studio/vol1/1
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=usagi.one
+// @grant        GM_xmlhttpRequest
+// @connect      target.com
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  GM_xmlhttpRequest({
+    method: "GET",
+    url: "https://web.usagi.one/",
+    headers: {
+      Referer: "https://google.com/",
+    },
+    onload: function (res) {
+      console.log("Status:", res.status);
+      console.log("Response:", res.responseText);
+    },
+    onerror: function (err) {
+      console.error("GM_xmlhttpRequest error:", err);
+    },
+  });
+})();
